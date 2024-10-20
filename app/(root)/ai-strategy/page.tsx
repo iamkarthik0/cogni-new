e57dbb198b4b1773d59ecbdda2c49fc2be1fe42a
skiller70 <singhlucky007007@gmail.com>
2024-10-20 11:43:04 +0530

    fix ai-strategy

---
+++ app/(root)/ai-strategy/page.tsx
@@ -0,0 +1,14 @@
+import StrategyHero from '@/components/shared/strategyPage/StrategyHero'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    
+        <StrategyHero/>
+    
+     </>
+  )
+}
+
+export default page
