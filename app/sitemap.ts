6b3f20f70f56f9b99e174432b4c9b3d38ae03409
skiller70 <singhlucky007007@gmail.com>
2024-10-21 23:24:40 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,154 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://cogninest.ai/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://cogninest.ai/healthcare",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+      url: "https://cogninest.ai/pharma",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: "https://cogninest.ai/cpg-industry",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+        url: "https://cogninest.ai/media",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/retail",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "https://cogninest.ai/ai-manufacturing",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/ai-finance",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "https://cogninest.ai/gen-ai-services",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/ml-dl-service",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/data-engineering",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/predictive-modeling",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/gpt-integration",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/document-ai-services",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "https://cogninest.ai/cloud-consultant",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/computer-vision",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/deep-learning",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/next-gen",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "https://cogninest.ai/idp",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "https://cogninest.ai/about-us",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "https://cogninest.ai/contact-us",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+
+      {
+        url: "https://cogninest.ai/blogs",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+  ];
+}
+
+// url: "",
